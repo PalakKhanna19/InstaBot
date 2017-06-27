30c00cf32474a6af66e48c2b47c5eebfbbbd8c7e
palak <khanna@gmail.com>
2017-06-27 11:49:27 +0530

    Added own media and media of user

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import requests
+import requests,urllib
 
-#APP_ACCESS_TOKEN = '414289191.86a6757.fd642c31a292425992c1eeb68dfab618'
+myAPP_ACCESS_TOKEN = '414289191.86a6757.fd642c31a292425992c1eeb68dfab618'
 APP_ACCESS_TOKEN ='5629236876.1cc9688.86db895c038043b5960dc2949785299a'
 BASE = 'https://api.instagram.com/v1/'
 
@@ -58,7 +58,84 @@ def get_user_info(insta_username):
     else:
         print 'Status code other than 200 received!'
 
-self_info()
-insta_username = raw_input("Enter the username of the user: ")
-get_user_id(insta_username)
-get_user_info(insta_username)
+def get_own_post():
+    request_url = (BASE+ 'users/self/media/recent/?access_token=%s') % (APP_ACCESS_TOKEN)
+    own_media = requests.get(request_url).json()
+
+    if own_media['meta']['code'] == 200:
+        if len(own_media['data']):
+            image_name = own_media['data'][0]['id'] + '.jpeg'
+            image_url = own_media['data'][0]['images']['standard_resolution']['url']
+            urllib.urlretrieve(image_url, image_name)
+            print 'Your image has been downloaded!'
+        else:
+            print 'Post does not exist!'
+    else:
+        print 'Status code other than 200 received!'
+
+def get_user_post(insta_username):
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print 'User does not exist!'
+        exit()
+    request_url = (BASE + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_media = requests.get(request_url).json()
+
+    if user_media['meta']['code'] == 200:
+        if len(user_media['data']):
+            image_name = user_media['data'][0]['id'] + '.jpeg'
+            image_url = user_media['data'][0]['images']['standard_resolution']['url']
+            urllib.urlretrieve(image_url, image_name)
+            print 'Your image has been downloaded!'
+        else:
+            print 'Post does not exist!'
+    else:
+        print 'Status code other than 200 received!'
+
+""""""
+def get_own_like():
+    request_url = (BASE+ 'users/self/media/liked?access_token=%s') % (myAPP_ACCESS_TOKEN)
+    my_like = requests.get(request_url).json()
+    if my_like['meta']['code'] == 200:
+        if len(my_like['data']):
+            likes = my_like['data'][0]['likes']
+            print likes
+""""""
+def start_bot():
+    while True:
+        print '\n'
+        print 'Welcome to InstaBot!'
+        print 'Chose from the following menu options:'
+        print "1.See your own details\n"
+        print "2.See details of a user by username\n"
+        print "3.Get your own recent post\n"
+        print "4.Get the recent post of a user by username\n"
+        #print "5.Get the user who has liked your  post\n"
+
+        print "6.Leave InstaBot"
+
+        choice = raw_input("Enter you choice: ")
+        if choice == "1":
+            self_info()
+        elif choice == "2":
+            insta_username = raw_input("Enter the username of the user: ")
+            get_user_info(insta_username)
+        elif choice == "3":
+            get_own_post()
+        elif choice == "4":
+            insta_username = raw_input("Enter the username of the user: ")
+            get_user_post(insta_username)
+
+       # elif choice == "5":
+            #get_own_like()
+
+
+        elif choice == "6":
+            exit()
+        else:
+            print "wrong choice"
+
+start_bot()
+
+
